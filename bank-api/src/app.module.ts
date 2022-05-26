71414851b0b60abe2b64ab83a3d5268171c97216
Victor Borsatto <victor.borsattobh@gmail.com>
2022-05-25 21:36:11 -0300

    Created bank account and pix key on controllers, models and database

--- bank-api/src/app.module.ts
+++ bank-api/src/app.module.ts
@@ -1,10 +1,43 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BankAccount } from './models/bank-account.model';
+import { BankAccountController } from './controllers/bank-account/bank-account.controller';
+import { ConsoleModule } from 'nestjs-console';
+import { FixturesCommand } from './fixtures/fixtures.command';
+import { PixKeyController } from './controllers/pix-key/pix-key.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    ConsoleModule,
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION as any,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [BankAccount],
+    }),
+    TypeOrmModule.forFeature([BankAccount]),
+    ClientsModule.register([
+      {
+        name: 'CODEPIX_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          url: process.env.GRPC_URL,
+          package: 'github.com.vicbvs.codepix',
+          protoPath: [join(__dirname, 'protofiles/pixkey.proto')],
+        },
+      },
+    ]),
+  ],
+  controllers: [AppController, BankAccountController, PixKeyController],
+  providers: [AppService, FixturesCommand],
 })
 export class AppModule {}
---
+++ bank-api/src/models/bank-account.model.ts
@@ -0,0 +1,48 @@
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { v4 as uuid4 } from 'uuid';
+
+@Entity({
+  name: 'bank_accounts',
+})
+export class BankAccount {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  account_number: string;
+
+  @Column()
+  owner_name: string;
+
+  @Column()
+  balance: number;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  created_at: Date;
+
+  @BeforeInsert()
+  generateId() {
+    if (this.id) {
+      return;
+    }
+
+    this.id = uuid4();
+  }
+
+  @BeforeInsert()
+  initBalance() {
+    if (this.balance) {
+      return;
+    }
+
+    this.balance = 0;
+  }
+}
